55085b133a1a3cc73260e73ac48cb419892cd946
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-13 21:32:45 +0300

    refactor: remove <base>

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,13 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/` directory.
+  // Load Markdown and MDX files in the `/content/` directory.
   loader: glob({ base: "./content/", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
-    date: z.coerce.date().optional(),
+    // date: z.coerce.date().optional(),
   }),
 });
 
