7177d649301603ecbb73662ef68ec2879f2f4397
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-17 12:52:39 +0300

    use config

--- src/consts.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_URL = "pona.la";
-export const SITE_TITLE = "pona.la: a domain for Toki Pona";
-export const SITE_DESCRIPTION = "A domain for Toki Pona projects.";
-export const GOATCOUNTER_URL = "pona-la.goatcounter.com";
